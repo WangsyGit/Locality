0e3bf4da2544bd810a7e8373663558b89ea28713
lllallla <14302010051@fudan.edu.cn>
2016-02-04 20:38:17 +0800

    2-4
    HUYI & WEINA  improvement needed

--- Nearby/app/src/main/java/stu/f40503/nearby/MainActivity.java
+++ Nearby/app/src/main/java/stu/f40503/nearby/MainActivity.java
@@ -1,9 +1,15 @@
 package stu.f40503.nearby;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +17,47 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.bluetooth);
+
+        BluetoothAdapter bta;
+        bta = BluetoothAdapter.getDefaultAdapter();
+        TextView status = (TextView)findViewById(R.id.bluetooth);
+        if (!bta.isEnabled()) {
+            bta.enable();
+        }
+        status.setText("蓝牙已开启");
+        /*
+        *
+        * connect the bluetooth
+        *
+        * &
+        *
+        * determine whether the bluetooth has correctly connected
+        *
+        * */
+        Button enter = (Button)findViewById(R.id.enter);
+        enter.setEnabled(true);
+        Button.OnClickListener listener = new Button.OnClickListener() {
+            public void onClick (View v) {
+                Intent intent=new Intent(MainActivity.this,CameraActivity.class);
+                startActivity(intent);
+                /*
+                *
+                * move to the
+                *
+                * main page
+                *
+                *
+                *
+                *
+                *
+                *
+                * */
+            }
+        };
+        enter.setOnClickListener(listener);
     }
 
     @Override
